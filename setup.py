015ec3ef761d827ca22e56726ccad69fbceae99d
Matt Layman <matthewlayman@gmail.com>
2014-05-03 22:51:34 -0400

    Update setup.py for trove classifiers.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2014, Matt Layman
 '''
 handroll development is on `GitHub <https://github.com/mblayman/handroll>`_.
+
+handroll is a static website generator that uses Markdown as the source format.
 '''
 
 from setuptools import find_packages, setup
@@ -28,7 +30,7 @@ if __name__ == '__main__':
         license='BSD',
         author='Matt Layman',
         author_email='matthewlayman@gmail.com',
-        description='Website development is a finely crafted art.',
+        description='A website generator for software artisans',
         long_description=__doc__,
         packages=find_packages(),
         entry_points={
@@ -36,5 +38,29 @@ if __name__ == '__main__':
         },
         include_package_data=True,
         zip_safe=False,
+        platforms='any',
         install_requires=install_requires,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Topic :: Artistic Software',
+            'Topic :: Documentation',
+            'Topic :: Internet :: WWW/HTTP :: Site Management',
+            'Topic :: Office/Business :: News/Diary',
+            'Topic :: Software Development :: Documentation',
+            'Topic :: Text Processing :: Markup :: HTML',
+        ],
+        keywords=[
+            'Markdown',
+            'generator',
+        ],
     )
