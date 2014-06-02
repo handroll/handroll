0adc25f26928c344befb8abc267918cf691c97a5
Matt Layman <matthewlayman@gmail.com>
2014-06-01 21:15:46 -0400

    Add PyPy classifier.

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: Implementation :: PyPy',
             'Topic :: Artistic Software',
             'Topic :: Documentation',
             'Topic :: Internet :: WWW/HTTP :: Site Management',
